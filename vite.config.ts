459bfd9f02346eebb3233907cf41749a8e769154
HuttyBuddy <kevinhutty@gmail.com>
2025-10-08 09:11:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({ base: "/", plugins: [react()] });
 
-// IMPORTANT: base must be '/' so assets load correctly on Cloud Run
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-})
