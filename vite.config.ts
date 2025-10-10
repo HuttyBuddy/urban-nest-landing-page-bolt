b72e7997cb275ffda6b2850e8f156cb15ccadfc0
HuttyBuddy <kevinhutty@gmail.com>
2025-10-10 10:40:34 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+// Explicitly load .env file
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
